d3e8e1fb27f622862a8ed0fc0f452c2aa95eb395
techux <connect.world12345@gmail.com>
2025-10-30 21:31:35 +0530

    Refactor App component to include Navbar, Hero, Showcase, Features, Pricing, Cta, and Footer for improved layout and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Hero from "./components/HeroSection/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Showcase from "./components/Showcase/Showcase";
+import Pricing from "./components/Pricing/Pricing";
+import Cta from "./components/Cta/Cta";
+import Footer from "./components/Footer/Footer";
+import Features from "./components/Features/Features";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Showcase />
+      <Features />
+      <Pricing />
+      <Cta />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
